24a2aef237ef1397c99ea894b8cad4ecbaf05371
Geedhavarshini <geedhavarshinii@gmail.com>
2023-12-03 17:23:59 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+#make a virtual env and install all the modules needed
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+#make a route and render all the html templates in this route
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if requests.method == 'POST':
+        city_name = request.form.get('city')
+
+        #take a variable to show the json data
+        r = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+city_name+'&appid=cbf1b9f6f3127d65b15aa57c0cd3d28a')
+        json_object = r.json()
+
+        #take some attributes like temperature, humidity, pressur of this
+        temperature = int(json_object['main']['temp']-273.15) #this temperature in kelvin
+        humidity = int(json_object['main']['humidity'])
+        pressure = int(json_object['main']['pressure'])
+        wind = int(json_object['wind']['speed'])
+
+        #finally just pass the variables
+        condition = json_object['weather'][0]['main']
+        desc = json_object['weather'][0]['description']
+        
+        return render_template('home.html', temperature=temperature, pressure=pressure, humidity=humidity, city_name=city_name, condition=condition, wind=wind, desc=desc)
+    else:
+        return render_template('home.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
